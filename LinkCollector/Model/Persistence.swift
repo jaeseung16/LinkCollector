9eda2a15fa4ef7dccd0ab48568980a017bbe7aaa
Jae-Seung Lee <jaeseung@gmail.com>
2021-08-23 19:40:53 -0400

    Feature: Replace fatalError with Logger

--- LinkCollector/Model/Persistence.swift
+++ LinkCollector/Model/Persistence.swift
@@ -6,9 +6,11 @@
 //
 
 import CoreData
+import os
 
 struct PersistenceController {
     static let shared = PersistenceController()
+    static let logger = Logger()
 
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
@@ -20,10 +22,8 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            PersistenceController.logger.error("Unresolved error \(nsError), \(nsError.userInfo)")
         }
         return result
     }()
@@ -38,11 +38,9 @@ struct PersistenceController {
         } else {
             
             guard let applicationSupportPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).last else {
-                       fatalError("Shared file container could not be created.")
+                PersistenceController.logger.error("Shared file container could not be created")
+                return
             }
-            
-            //let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
-            print("applicationSupportPath = \(applicationSupportPath)")
             
             let cloudStoreURL = applicationSupportPath.appendingPathComponent("LinkCollector/LinkCollector.sqlite")
             let cloudStoreDescription = NSPersistentStoreDescription(url: cloudStoreURL)
@@ -56,18 +54,7 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                PersistenceController.logger.error("Could not load persistent store: \(storeDescription), \(error), \(error.userInfo)")
             }
         })
         
@@ -85,7 +72,9 @@ struct PersistenceController {
         do {
             try container.newBackgroundContext().execute(purgeHistoryRequest)
         } catch {
-            print("Could not purge history: \(error)")
+            if let error = error as NSError? {
+                PersistenceController.logger.error("Could not purge history: \(error), \(error.userInfo)")
+            }
         }
     }
     
@@ -95,10 +84,9 @@ struct PersistenceController {
             do {
                 try context.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                if let error = error as NSError? {
+                    PersistenceController.logger.error("Could not save: \(error), \(error.userInfo)")
+                }
             }
         }
     }
