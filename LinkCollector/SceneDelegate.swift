c0f4e279d5a624fec414ebeaddcdec894e348683
Jae-Seung Lee <jaeseung@gmail.com>
2023-08-26 14:22:34 -0400

    Refactor: Remove managedObjectContext from views

--- LinkCollector/SceneDelegate.swift
+++ LinkCollector/SceneDelegate.swift
@@ -34,7 +34,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
         let contentView = ContentView()
-            .environment(\.managedObjectContext, persistence.container.viewContext)
             .environmentObject(viewModel)
 
         // Use a UIHostingController as window root view controller.
