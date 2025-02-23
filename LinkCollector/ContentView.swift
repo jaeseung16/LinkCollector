5dbb5c67d6e553974419d7390ba2ff799cf2c1fc
Jae-Seung Lee <jaeseung@gmail.com>
2025-02-23 13:34:10 -0500

    refactor: Remove unused properties

--- LinkCollector/ContentView.swift
+++ LinkCollector/ContentView.swift
@@ -11,30 +11,6 @@ import MapKit
 struct ContentView: View {
     @EnvironmentObject var viewModel: LinkCollectorViewModel
     
-    let calendar = Calendar(identifier: .iso8601)
-    let today = Date()
-
-    private var todayStartOfDay: Date {
-        calendar.startOfDay(for: today)
-    }
-    
-    private var sevenDaysAgo: Date {
-        return calendar.date(byAdding: .day, value: -7, to: today)!
-    }
-    
-    private var firstDayOfMonth: Date {
-        let daysOfMonth = calendar.component(.day, from: today)
-        return calendar.date(byAdding: .day, value: -daysOfMonth, to: today)!
-    }
-    
-    private var thisWeek: DateInterval {
-        return DateInterval(start: sevenDaysAgo, end: todayStartOfDay)
-    }
-    
-    private var thisMonth: DateInterval {
-        return DateInterval(start: firstDayOfMonth, end: sevenDaysAgo)
-    }
-    
     @State private var selectedLink: LinkEntity?
     
     var body: some View {
@@ -51,7 +27,6 @@ struct ContentView: View {
                 }
             }
         }
-        
     }
     
 }
